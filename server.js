c9fa670cc504711fe12f37c737f4e954ee6e5712
LinxPhy <lin.aboagye@gmail.com>
2023-04-15 10:47:52 +0100

    url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://anime-sanctuary.netlify.app",
         methods: ["GET", "POST"]
     })
 )
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 import './App.css'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://anime-sanctuary.netlify.app/api',
 })
 
 function App() {
